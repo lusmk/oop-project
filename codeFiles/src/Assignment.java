aff59456bc3361056e62ff891a1ebe5fd1f1fb8d
Shogher01 <shogher.galstyan@mail.ru>
2022-04-12 19:01:05 +0400

    Fixes in Assignment and Course classes

--- codeFiles/src/Assignment.java
+++ codeFiles/src/Assignment.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Assignment {
+public class Assignment {
+    // instance variable
+    private int numberOfAssignments;
+
+    // no-arg constructor
+    public Assignment() {
+        numberOfAssignments = 0;
+    }
+
+    // second constructor
+    public Assignment(int numberOfAssignments) {
+        this.numberOfAssignments = numberOfAssignments;
+    }
+
+    //copy constructor
+    public Assignment(Assignment assignment) {
+        this.numberOfAssignments = assignment.numberOfAssignments;
+    }
+
+    //accessor for number of assignment
+    public int getNumberOfAssignments() {
+        return this.numberOfAssignments;
+    }
 }
--- codeFiles/src/Course.java
+++ codeFiles/src/Course.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Course {
+public class Course {
+    //instance variables
+    private Major name;
+    private int credits;
+    private Assignment assignment;
+
+    //no-arg constructor
+    public Course() {
+        name = new Major();
+        credits = 0;
+        assignment = new Assignment();
+    }
+
+    //second constructor
+    public Course(Major name, int credits, Assignment assignment) {
+        this.name = new Major(name);
+        this.credits = credits;
+        this.assignment = new Assignment(assignment);
+    }
+
+    //copy constructor
+    public Course(Course course) {
+        this.name = new Major(course.name);
+        this.credits = course.credits;
+        this.assignment = new Assignment(course.assignment);
+    }
+
+    //accessor for major name
+    public Major getName() {
+        return new Major(this.name);
+    }
+
+    // accessor for credits
+    public int getCredits() {
+        return this.credits;
+    }
+
+    // accessor for assignment
+    public Assignment getAssignment() {
+        return new Assignment(this.assignment);
+    }
+
 }
