c27c29eccbf21bc9ae5964ef97d5e781b82eddf0
Shogher01 <shogher.galstyan@mail.ru>
2022-04-12 21:40:10 +0400

    Added implementation for Student and Instructor classes

---
+++ codeFiles/src/Instructor.java
@@ -0,0 +1,23 @@
+public class Instructor extends Person {
+    Course teachingSubject;
+
+    //no-arg constructor
+    public Instructor() {
+        super();
+        teachingSubject = new Course(new Major(), 3, new Assignment());
+    }
+
+    public Instructor(String name, Course teachingSubject) {
+        super(name);
+        this.teachingSubject = teachingSubject;
+    }
+
+    public Instructor(Instructor instructor) {
+        super(instructor);
+        teachingSubject = instructor.teachingSubject;
+    }
+
+    public Course getTeachingSubject() {
+        return teachingSubject;
+    }
+}
--- codeFiles/src/Student.java
+++ codeFiles/src/Student.java
@@ -1,16 +1,43 @@
 public class Student extends Person {
-    // private Major major;
+    private Major major;
     private int semester;
     private Degree degree;// maybe will be changed
 
     //Constructors
-    //non-arg(not sure if needed)
+    //no-arg(not sure if needed)
     public Student() {
         super();
         semester = 1;
         degree = Degree.UNDERGRADUATE;
+        major = new Major("Computer Science");
+    }
+
+    public Student(String name, Major major, Degree degree) {
+        super(name);
+        this.major = new Major(major);
+        this.degree = degree;
+        semester = 1;
+    }
+
+    //copy-constructor(not sure if needed)
+    public Student(Student student){
+        super(student);
+        this.major = student.major;
+        this.degree = student.degree;
+        this.semester = student.semester;
     }
 
 
+    //getters
+    public Major getMajor() {
+        return major;
+    }
 
+    public int getSemester() {
+        return semester;
+    }
+
+    public Degree getDegree() {
+        return degree;
+    }
 }
