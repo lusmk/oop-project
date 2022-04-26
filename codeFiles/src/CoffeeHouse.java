433bc43f0bde214e4461077dc3cac985b072f928
lusmk <lusine_mkrtchyan3@edu.aua.am>
2022-04-27 00:07:55 +0400

    added abstract class Place, its subclasses Cafeteria, CoffeeHouse

--- codeFiles/src/CoffeeHouse.java
+++ codeFiles/src/CoffeeHouse.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class CoffeeHouse {
+public class CoffeeHouse extends Place{
+
+    public CoffeeHouse(String name, String[] menu){
+        super(name, menu);
+    }
+
+    public void getOrder(){
+        System.out.println("Hi, I am " + this.getWorkersName() + ". What drink would you like?");
+    }
+
 }
