7ecfe22a2132d22296f6837c74cbe4d259fe6d6e
lusmk <lusine_mkrtchyan3@edu.aua.am>
2022-05-10 11:04:31 +0400

    added sleep button in EnergyPanel

--- codeFiles/src/Views/EnergyPanel.java
+++ codeFiles/src/Views/EnergyPanel.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class EnergyPanel extends JPanel {
 
     public EnergyPanel(){
-        super(new GridLayout(1,2));
+        super(new GridLayout(1,3));
         JButton cafeteriaButton = new JButton();
         ImageIcon cafeteria = new ImageIcon("codeFiles/src/Views/aries.png");
         Image image = cafeteria.getImage();
@@ -26,5 +26,15 @@ public class EnergyPanel extends JPanel {
         coffeeHouseButton.setFocusable(false);
         coffeeHouseButton.setPreferredSize(new Dimension(70,60));
         add(coffeeHouseButton);
+
+        JButton sleepButton = new JButton();
+        ImageIcon sleep = new ImageIcon("codeFiles/src/Views/sleep.png");
+        Image imageIcon= sleep.getImage();
+        Image newSleepImage = imageIcon.getScaledInstance(70, 60,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+        sleep = new ImageIcon(newSleepImage);
+        sleepButton.setIcon(sleep);
+        sleepButton.setFocusable(false);
+        sleepButton.setPreferredSize(new Dimension(70,60));
+        add(sleepButton);
     }
 }
